e86d67e491f7b97508e55b18e97add3b8bd8cf6e
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-23 11:02:28 -0400

    Add files via upload

---
+++ 10_5_employee.sql
@@ -0,0 +1,8 @@
+-- *** SqlDbx Personal Edition ***
+-- !!! Not licensed for commercial use beyound 90 days evaluation period !!!
+-- For version limitations please check http://www.sqldbx.com/personal_edition.htm
+-- Number of queries executed: 291, number of rows retrieved: 1234
+
+SELECT * FROM salary;
+SELECT * FROM employee;
+SELECT * FROM emp_leave;
